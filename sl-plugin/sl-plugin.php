d4b0c866ce52911055dbf7db0daf683a09b2c931
Alexey Loubyansky <olubyans@redhat.com>
2017-10-21 23:41:42 +0200

    cpt and the settings page

--- sl-plugin/sl-plugin.php
+++ sl-plugin/sl-plugin.php
@@ -17,32 +17,60 @@ defined( 'ABSPATH' ) or die;
 
 class SlPlugin {
 
+    protected $slCssPath = '/assets/sl.css';
+    protected $slJsPath = '/assets/sl.js';
+    protected $slSlug = 'sl_plugin';
+
+    protected $pluginName;
+
     function __construct() {
+        $this->pluginName = plugin_basename( __FILE__ );
     }
 
-    function activate() {
+    function register() {
+        register_activation_hook( __FILE__, [$this, 'activate'] );
+        register_deactivation_hook( __FILE__, [$this, 'deactivate'] );
+        add_action('init', [$this, 'custom_post_type'] );
+        add_action( 'admin_enqueue_scripts', [$this, 'enqueue'] );
+        add_action( 'admin_menu', [$this, 'add_admin_pages'] );
+
+        add_filter( "plugin_action_links_$this->pluginName", [$this, 'settings_link'] );
+    }
+
+    public function activate() {
         flush_rewrite_rules();
     }
 
-    function deactivate() {
-
+    public function deactivate() {
     }
 
-    function uninstall() {
-
-    }
-
-    function init_cpt() {
+    public function custom_post_type() {
         register_post_type( 'book', ['public' => true, 'label' => 'Vehicules'] );
+    }
+
+    public function add_admin_pages () {
+        add_menu_page( 'SL Plugin', 'SL', 'manage_options', $this->slSlug, [$this, 'admin_index'], 'dashicons-store', 110 );
+    }
+
+    public function admin_index() {
+        require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';
+    }
+
+    public function settings_link( $links ) {
+        array_push( $links, '<a href="admin.php?page=' . $this->slSlug . '">Settings</a>' );
+        return $links;
+    }
+
+    public function enqueue() {
+        wp_enqueue_style( 'slpluginstyle', plugins_url( $this->slCssPath, __FILE__ ) );
+        wp_enqueue_script( 'slpluginscript', plugins_url( $this->slJsPath, __FILE__ ) );
     }
 }
 
 
 if( class_exists( 'SlPlugin' ) ) {
     $slPlugin = new SlPlugin();
+    $slPlugin->register();
 }
 
-register_activation_hook( __FILE__, array( $slPlugin, 'activate' ) );
-register_deactivation_hook( __FILE__, array( $slPlugin, 'deactivate' ) );
-add_action('init', [$slPlugin, 'init_cpt']);
 
---
+++ sl-plugin/uninstall.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Plugin uninstall
+ *
+ * @package SlPlugin
+ */
+
+if( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    die;
+}
+
+$vehicules = get_posts( array( 'post_type' => 'vehicule', 'numberposts' => -1 ) );
+foreach( $vehicule as $vehicules ) {
+    wp_delete_post( $vehicule->ID, true );
+}
+
+global $wpdb;
+$wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'vehicule'" );
+$wpdb->query( "DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)" );
+$wpdb->query( "DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT id FROM wp_posts)" );
