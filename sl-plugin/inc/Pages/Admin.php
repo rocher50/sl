9da006a6c25c0d1da19357b967452bcc4f0213c4
Alexey Loubyansky <olubyans@redhat.com>
2017-11-04 22:02:46 +0100

    modular admin page

--- sl-plugin/inc/Pages/Admin.php
+++ sl-plugin/inc/Pages/Admin.php
@@ -14,6 +14,7 @@ class Admin extends BaseController {
 
     public $settings;
     public $pages;
+    public $subpages;
 
     public function __construct() {
         $this->settings = new SettingsApi();
@@ -26,9 +27,33 @@ class Admin extends BaseController {
             'icon_url' => 'dashicons-store',
             'position' => 110]
         ];
+
+        $this->subpages = [
+            ['parent_slug' => 'sl_plugin',
+            'page_title' => 'Custom Post Types',
+            'menu_title' => 'CPT',
+            'capability' => 'manage_options',
+            'menu_slug' => 'sl_cpt',
+            'callback' => function() { echo '<h1>CPT Manager</h1>'; }],
+
+            ['parent_slug' => 'sl_plugin',
+            'page_title' => 'Custom Taxonomies',
+            'menu_title' => 'Taxonomies',
+            'capability' => 'manage_options',
+            'menu_slug' => 'sl_taxonomies',
+            'callback' => function() { echo '<h1>Taxonomies Manager</h1>'; }],
+
+            ['parent_slug' => 'sl_plugin',
+            'page_title' => 'Custom Widgets',
+            'menu_title' => 'Widgets',
+            'capability' => 'manage_options',
+            'menu_slug' => 'sl_widgets',
+            'callback' => function() { echo '<h1>Widgets Manager</h1>'; }]
+
+        ];
     }
 
     public function register() {
-        $this->settings->add_pages( $this->pages )->register();
+        $this->settings->add_pages( $this->pages )->with_subpage('Dashboard')->add_subpages($this->subpages)->register();
     }
 }
