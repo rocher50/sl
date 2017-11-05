85a46dfd642d0b112354fc44acdfea3a3af57dda
Alexey Loubyansky <olubyans@redhat.com>
2017-11-05 23:22:19 +0100

    fixes for the calendar

--- sl-plugin/inc/Api/Calendar.php
+++ sl-plugin/inc/Api/Calendar.php
@@ -9,20 +9,20 @@ class Calendar {
 
     public function renderMonth($tstamp, $month_offset, bool $current_day_available) {
 
+        echo '<div class="calendar">';
         echo '<div class="month">';
         echo '  <ul>';
 
         if($month_offset  < 1) {
-            echo '  <li class="prev">&#10094;</li>';
+            echo '  <li class="arrow">&#10094;</li>';
         } elseif($month_offset == 1) {
-            echo '  <li class="prev"><a href="index.php">&#10094;</a></li>';
+            echo '  <li class="arrow"><a href="index.php">&#10094;</a></li>';
         } else {
-            echo '  <li class="prev"><a href="index.php?cal_mo=' . ($month_offset - 1) . '">&#10094;</a></li>';
+            echo '  <li class="arrow"><a href="index.php?cal_mo=' . ($month_offset - 1) . '">&#10094;</a></li>';
         }
 
-        echo '    <li class="next"><a href="index.php?cal_mo=' . ($month_offset + 1) . '">&#10095;</a></li>';
-
-        echo '    <li>' . date('F', $tstamp) . ', <span style="font-size:18px">' . date('Y', $tstamp ) . '</span></li>';
+        echo '    <li>' . date('F', $tstamp) . ', ' . date('Y', $tstamp ) . '</li>';
+        echo '    <li class="arrow"><a href="index.php?cal_mo=' . ($month_offset + 1) . '">&#10095;</a></li>';
         echo '  </ul>';
         echo '</div>';
 
@@ -35,13 +35,14 @@ class Calendar {
         echo '  <li>Sa</li>';
         echo '  <li>Di</li>';
         echo '</ul>';
+        echo '<div class="after_box"/>';
 
         echo '<ul class="days">';
         $month_day_start = idate('w', mktime(0, 0, 0, idate('m', $tstamp), 1, idate('Y', $tstamp)));
         $i = 0;
         while($i < $month_day_start - 1) {
             $i++;
-            echo '<li/>';
+            echo '<li class="blank"/>';
         }
 
         $first_available_day = idate('d', $tstamp);
@@ -51,15 +52,17 @@ class Calendar {
         $i = 0;
         while($i < $first_available_day) {
             $i++;
-            echo '<li>' . $i . '</li>';
+            echo '<li class="day-na">' . $i . '</li>';
         }
 
         $days_in_month = idate('t', $tstamp);
         while($i < $days_in_month) {
             $i++;
-            echo '<li><span class="na"><a href="#">' . $i . '</span></a></li>';
+            echo '<li class="day-av"><a href="#">' . $i . '</a></li>';
         }
         echo '</ul>';
+        echo '<div class="after_box"/>';
+        echo '</div>';
     }
 }
 
