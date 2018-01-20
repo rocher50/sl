d2aeb5263612eb7121109c5a7566b68b78899086
Alexey Loubyansky <olubyans@redhat.com>
2018-01-20 23:51:42 +0100

    added custom checkbox and refactoring

--- plugins/sl-plugin/inc/Api/Callbacks/ManagerCallbacks.php
+++ plugins/sl-plugin/inc/Api/Callbacks/ManagerCallbacks.php
@@ -20,6 +20,6 @@ class ManagerCallbacks extends BaseController {
         $name = $args['label_for'];
         $class = $args['class'];
         $checkbox = get_option($name);
-        echo '<input type="checkbox" name="' . $name . '" value="1" class="' . $class . '" ' . ($checkbox ? 'checked' : '') . '>';
+        echo '<div class="' . $class . '"><input type="checkbox" name="' . $name . '" id="' . $name . '" value="1" class="" ' . ($checkbox ? 'checked' : '') . '><label for="' . $name . '"><div></div></label></div>';
     }
 }
--- plugins/sl-plugin/inc/Base/BaseController.php
+++ plugins/sl-plugin/inc/Base/BaseController.php
@@ -11,12 +11,25 @@ class BaseController {
     public $plugin_path;
     public $plugin_url;
     public $plugin;
+    public $managers = [];
 
     public function __construct() {
         $this->plugin_path = plugin_dir_path( dirname( __FILE__, 2 ) );
         $this->plugin_url = plugin_dir_url( dirname( __FILE__, 2 ) );
         $this->plugin = plugin_basename( dirname( __FILE__, 3 ) );
         $this->plugin = $this->plugin . '/' . $this->plugin . '.php';
+
+        $this->managers = [
+            'cpt_manager' => 'CPT Manager',
+            'taxonomy_manager' => 'Taxonomy Manager',
+            'media_widget' => 'Media Widget',
+            'gallery_manager' => 'Gallery Manager',
+            'testimonial_manager' => 'Testimonial Manager',
+            'templates_manager' => 'Templates Manager',
+            'login_manager' => 'Login Manager',
+            'membership_manager' => 'Membership Manager',
+            'chat_manager' => 'Chat Manager'
+        ];
     }
 }
 
